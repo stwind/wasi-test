ab6e704ebfff41040b65b8361ec1d7761c566551
stwind <stwindfy@gmail.com>
2025-01-01 15:59:48 +0800

    use stdlib

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #define EXPORT(NAME) __attribute__((export_name(#NAME))) NAME
+#define IMPORT(NAME) __attribute__((import_module("env"), import_name(#NAME))) NAME
 
 int EXPORT(add)(int a, int b) {
     return a + b;
--- wasi.test.ts
+++ wasi.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test } from 'bun:test';
 import { readFileSync } from "node:fs";
 
-const instantiate = async <T,>(bin: Buffer, env: WebAssembly.ModuleImports) => {
-  const { instance } = await WebAssembly.instantiate(bin, { env });
+const instantiate = async <T,>(bin: Buffer, imports: WebAssembly.Imports) => {
+  const { instance } = await WebAssembly.instantiate(bin, imports);
   return instance.exports as unknown as T;
 };
 
@@ -10,7 +10,12 @@ const mod = await instantiate<{
   memory: WebAssembly.Memory;
   add(a: number, b: number): number;
 }>(readFileSync("./main.wasm"), {
-  memory: new WebAssembly.Memory({ initial: 10 }),
+  env: {
+    memory: new WebAssembly.Memory({ initial: 10 }),
+  },
+  wasi_snapshot_preview1: {
+    random_get: Math.random
+  }
 });
 
 describe("wasm", () => {
